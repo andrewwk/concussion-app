a2311231bb6604d6a25caa5cdb64a0dcce821f8d
Andrew Kim <awkim.11@gmail.com>
2017-02-27 15:29:02 -0500

    Added body parser dependency.

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 
 const express      = require('express');
-const app          = express();
+const bodyParser   = require('body-parser');
 const ENV          = process.env.ENV || "development";
 const PORT         = process.env.PORT || 8080
+const app          = express();
+
+// parse application/json
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get('/', (req, res) => res.status(200).send('app connected and made get request to root.'));
 
-app.get('/webhook', (req, res) => res.status(200).send('app connected and made get request to root.'));
+app.get('/webhook', (req, res) => res.status(200).send(''));
+
+app.post('/webhook', (req, res) => {
+
+})
 
 app.use((req, res) => res.status(404).send('Error 404. This path does not exist.'));
 
