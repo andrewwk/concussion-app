dfa2631cbd6eeb0f8ea42af93210fa75f86a29fe
Andrew Kim <awkim.11@gmail.com>
2017-02-27 15:38:07 -0500

    Added routes for webhooks.

--- server.js
+++ server.js
@@ -9,16 +9,39 @@ const app          = express();
 // parse application/json
 app.use(bodyParser.json())
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => res.status(200).send('app connected and made get request to root.'));
 
-app.get('/webhook', (req, res) => res.status(200).send(''));
+// app.get('/webhook', (req, res) => res.status(200).send(''));
+//
+// app.post('/webhook', (req, res) => {
+// })
 
+/* For Facebook Validation */
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.FB_PAGE_ACCESS_TOKEN) {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
+
+/* Handling all messenges */
 app.post('/webhook', (req, res) => {
-
-})
+  console.log(req.body);
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
 
 app.use((req, res) => res.status(404).send('Error 404. This path does not exist.'));
 
