cf10b7e3a9d39f52bdd6c3cde98acb98c45c70b4
Andrew Kim <awkim.11@gmail.com>
2017-02-27 16:13:24 -0500

    In the process of correcting unwanted file tracking.

--- server.js
+++ server.js
@@ -2,9 +2,32 @@ require('dotenv').config();
 
 const express      = require('express');
 const bodyParser   = require('body-parser');
+const require      = require('request');
 const ENV          = process.env.ENV || "development";
 const PORT         = process.env.PORT || 8080
 const app          = express();
+
+const sendMessage = (event) => {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, (error, response) => {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 
 // parse application/json
 app.use(bodyParser.json())
