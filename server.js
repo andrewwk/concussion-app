aca055c8a6111c6cb2f2d013be64ceac77d9ae4b
Andrew Kim <awkim.11@gmail.com>
2017-02-27 14:53:17 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express      = require('express');
+const app          = express();
+const ENV          = process.env.ENV || "development";
+const PORT         = process.env.PORT || 8080
+
+app.get('/', (req, res) => res.status(200).send('app connected and made get request to root.'));
+
+app.use((req, res) => res.status(404).send('Error 404. This path does not exist.'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
