5ef8132e2623c846e3e1437c9f114d6f5ff86304
Dtoyne <dylantoyne@gmail.com>
2017-03-03 16:34:43 -0500

    Finished styling email (results) receipt

--- server.js
+++ server.js
@@ -53,6 +53,23 @@ const sendMessage = (event) => {
     });
   });
 
+    request({
+      url: 'https://graph.facebook.com/v2.6me/messages',
+      qs: {access_token: PAGE_ACCESS_TOKEN},
+      method: 'GET',
+      json: {
+        recipient: {id: sender}
+      }
+    }, (error, response) => {
+      if (error) {
+        // console.log('Error sending message: ', error);
+        console.log('shit');
+      } else if (response.body.error) {
+        // console.log('Error: ', response.body.error);
+        console.log('fuck');
+      }
+  });
+
   apiai.on('error', (error) => {
     console.log('ERROR', error);
   });
