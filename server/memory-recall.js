51319b8efc07dbcc0079b80bb5c61f1f69b5d7f3
Andrew Kim <awkim.11@gmail.com>
2017-03-05 17:04:49 -0500

    File contains function to score user answers for memory recall tests.

---
+++ server/memory-recall.js
@@ -0,0 +1,24 @@
+const stringArray  = ['elbow', 'apple', 'carpet', 'saddle', 'bubble'];
+const bobInput     = 'elbow apple';
+const aliceInput   = 'wine carpet chicken';
+const deandreInput = 'cadillac saddle bubble winterberry football'
+
+
+const matchStrings = (input) => {
+  const inputArr = input.split(' ');
+  let score = 0;
+  stringArray.map((val) => {
+    if (inputArr.includes(val)) {
+      score += 1
+    }
+  })
+  return score;
+}
+console.log('Bob');
+console.log(matchStrings(bobInput));
+
+console.log('Alice');
+console.log(matchStrings(aliceInput));
+
+console.log('Deandre');
+console.log(matchStrings(deandreInput));
