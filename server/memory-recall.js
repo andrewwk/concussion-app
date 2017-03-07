a51bd5286d68a243b1f563ed5216484b143dd614
Andrew Kim <awkim.11@gmail.com>
2017-03-06 20:14:49 -0500

    Changed memory recall score function. Converting user answer to array no longer neccessary since it already comes in as an array.

--- server/memory-recall.js
+++ server/memory-recall.js
@@ -4,12 +4,14 @@ const recallTestWords  = ['elbow', 'apple', 'carpet', 'saddle', 'bubble'];
 // Andrew - Function to check how many words the user can remember. One point given for each correct
 // word.
 const matchStrings = (input) => {
-  const inputArr = input.split(' ');
+  // const inputArr = input.split(' ');
   let score = 0;
   recallTestWords.map((val) => {
-    if (inputArr.includes(val)) {
-      score += 1
+    if (input.includes(val)) {
+      score += 1;
     }
   })
   return score;
 }
+
+module.exports = matchStrings;
