3b83b025e2f58193d885c5031d99ec2c072abc31
Andrew Kim <awkim.11@gmail.com>
2017-03-06 20:19:32 -0500

    User report object that will be inserted into the database.

---
+++ server/user-report.js
@@ -0,0 +1,20 @@
+const userReport = {
+  conversationID             : '',
+  firstName                  : '',
+  lastName                   : '',
+  email                      : '',
+  potentialSignsOfConcussion : [],
+  howDoYouFeel               : [],
+  numberOfSymptoms           : 0,
+  symptomSeverityScore       : 0,
+  sacOrientation             : [],
+  sacOrientationScore        : 0,
+  sacMemory                  : [],
+  sacMemoryScore             : 0,
+  sacConcentration           : [],
+  sacConcentrationScore      : 0,
+  sacDelayedRecallScore      : 0,
+  sacTotalScore              : 0
+}
+
+module.exports = userReport
