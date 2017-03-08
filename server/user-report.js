f30ec84d695d15f60caee7e5248ce9e0bee9758a
Andrew Kim <awkim.11@gmail.com>
2017-03-08 17:21:10 -0500

    Added create new user report function.

--- server/user-report.js
+++ server/user-report.js
@@ -18,38 +18,34 @@ const userReport = {
   sacTotalScore              : 0
 }
 
-module.exports = userReport;
+// module.exports = userReport;
 
+const userReports = {};
 
-// const emailReport = {
-//   conversationID :  {
-//     conversationID       : conversationID,
-//     testDate             : new Date(),
-//     numberOfSymptoms     : 0, //  Out of 22
-//     symptomSeverityScore : 0, // Out of 132
-//     orientation          : 0, //  Out of 5
-//     immediateMemory      : 0, // Out of 15
-//     concentration        : 0, // Out of 5
-//     delayedRecall        : 0, // Out of 5
-//     sacTotal             : 0, // Sum Scores
-//     answeredQuestions    : []
-//   }
-// }
-//
-// const conversations = {};
-//
-// const conversationInit = (id) => {
-//   conversations[id] =
-//     {
-//       conversationID       : id,
-//       testDate             : new Date(),
-//       numberOfSymptoms     : 0, //  Out of 22
-//       symptomSeverityScore : 0, // Out of 132
-//       orientation          : 0, //  Out of 5
-//       immediateMemory      : 0, // Out of 15
-//       concentration        : 0, // Out of 5
-//       delayedRecall        : 0, // Out of 5
-//       sacTotal             : 0, // Sum Scores
-//       answeredQuestions    : []
-//     }
-// }
+const userReportInit = (id) => {
+  userReports[id] =
+    {
+      conversationID             : '',
+      firstName                  : '',
+      lastName                   : '',
+      email                      : '',
+      potentialSignsOfConcussion : [],
+      howDoYouFeel               : [],
+      numberOfSymptoms           : 0,
+      symptomSeverityScore       : 0,
+      sacOrientation             : [],
+      sacOrientationScore        : 0,
+      sacMemory                  : [],
+      sacMemoryScore             : 0,
+      sacConcentration           : [],
+      sacConcentrationScore      : 0,
+      sacDelayedRecall           : [],
+      sacDelayedRecallScore      : 0,
+      sacTotalScore              : 0
+    }
+}
+
+module.exports = {
+  userReports    : userReports,
+  userReportInit : userReportInit
+}
