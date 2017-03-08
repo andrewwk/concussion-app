0c4d0fe62b4e94ee88a6df36ca01f060e62c586a
Andrew Kim <awkim.11@gmail.com>
2017-03-08 17:20:35 -0500

    Moved string variables into hash. Otherwise, function would work incorrectly.

--- server/concentration.js
+++ server/concentration.js
@@ -1,11 +1,13 @@
-const digitsQ1        = '3 9 4'
-const digitsQ2        = '4 1 8 3'
-const digitsQ3        = '1 7 9 2 6'
-const digitsQ4        = '2 6 4 8 1 7'
-const monthsBackwards = 'december november october september august july june may april march february january'
+const correctAnswer = {
+  digitsQ1        : '3 9 4',
+  digitsQ2        : '4 1 8 3',
+  digitsQ3        : '1 7 9 2 6',
+  digitsQ4        : '2 6 4 8 1 7',
+  monthsBackwards : 'december november october september august july june may april march february january'
+}
 
 const assessAnswer = (params, answer) => {
-  return params == answer;
+  return correctAnswer[params] == answer;
 }
 
 module.exports = assessAnswer;
